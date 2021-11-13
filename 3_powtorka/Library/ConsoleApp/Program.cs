8731fa094bff361e82b50a1f68b11436978fb569
Damian Kościanek <damian.koscianek@gmail.com>
2021-11-13 18:19:33 +0100

    książki

---
+++ 3_powtorka/Library/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Persistence;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Urok", "Mateusz Mękarski i Kaja Kraska", 2021, "978-83-66747-35-7", 5000, 49.90m);
+            BooksRepository repository = new BooksRepository();
+
+            Console.WriteLine("Podaj login: ");
+            string login = Console.ReadLine();
+            Console.WriteLine("Podaj hasło: ");
+            string haslo = Console.ReadLine();
+
+            if (login == "Admin" && haslo == "password")
+                Console.WriteLine("Access granted");
+            else
+                Console.WriteLine("Access denied");
+
+        }
+    }
+}
