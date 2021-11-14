de179b5abcd8f137025f844dda103bf4929a70cd
Damian Kościanek <damian.koscianek@gmail.com>
2021-11-14 17:28:18 +0100

    Zadanie 6: Orders punkt 12

---
+++ 3_powtorka/Library/Domain/BookOrdered.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class BookOrdered
+    {
+        int BookId { get; set; }
+        int NumerOrdered { get; set; }
+    }
+}
---
+++ 3_powtorka/Library/Domain/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain
+{
+    public class Order
+    {
+        DateTime Date { get; set; }
+        List<BookOrdered> BooksOrderedList { get; set; }
+
+        Order()
+        {
+            Date = DateTime.Now;
+            BooksOrderedList = new();
+        }
+        public override string ToString()
+        {
+            string str = $"Order: {Date} Book: {BookId} Count: {NumerOrdered}";
+            return str;
+        }
+    }
+}
